f300622f7b2c9f0bed860b6e8d1122eafe09ab09
uzeen <uzeen@gmail.com>
2018-07-17 00:41:51 +0530

    Added new home function

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000;
 app.get('/',(req,res)=>{
     res.send("<h1>Hi Uzeen Successfully rendered</h1>");
 });
+app.get('/home',(req,res)=>{
+    res.send("<h1>Welcome to home page</h1>");
+});
 app.listen(port,()=>{
     console.log(`Server started on port ${port}`);
 });
