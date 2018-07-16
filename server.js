4616098e90d441691ff3a0d5773fe7e6bbfb0c32
uzeen <uzeen@gmail.com>
2018-07-16 23:18:48 +0530

    init commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+var app = express();
+var hbs = require('hbs');
+const port = process.argv.PORT || 3000; 
+app.get('/',(req,res)=>{
+    res.send("<h1>Hi Uzeen Successfully rendered</h1>");
+});
+app.listen(port,()=>{
+    console.log(`Server started on port ${port}`);
+});
