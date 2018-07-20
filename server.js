24b495e4389b0e6f9918d8853faaeba2693589c2
uzeen <uzeen@gmail.com>
2018-07-21 03:56:39 +0530

    port added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var hbs  = require('hbs');
 var qs  = require("querystring");
 
 var app = express();
-const port = process.env.PORT | 3000 ;
+const port = process.env.PORT || 3000 ;
 app.use(express.static(__dirname + "/image"));
 app.get("/",(req,res) =>{
     res.render('home.hbs');
